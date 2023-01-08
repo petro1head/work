2d74bd096c0775ac98b74a327309e381cd8f09e1
petro1head <makeeva.1999ghbhjlf@gmail.com>
2023-01-08 14:27:45 +0300

    все блоки написаны на структурах

--- burk_func/burk_func.ino
+++ burk_func/burk_func.ino
@@ -1,82 +1,78 @@
+// структура сумматора
+typedef struct Sum {
+  // выходной сигнал от сумматора
+  double u_sum = 0;
+} Sum;
 
+// структура коэффициента усиления Gain
+typedef struct Gain {
+  // выходной сигнал от коэффициента усиления
+  double u_gain = 0;
+} Gain;
+
+// структура насыщения Saturation
+typedef struct Saturation {
+  // выходной сигнал от насыщения
+  double u_saturation = 0;
+} Saturation;
+
+// структура дискретного интегратора
 typedef struct Disc_Integ {
   // переменные используемые внутри дискретного интегратора
   double t_before = 0;
-  double u = 0;
+  // выходной сигнал от дискретного интегратора
+  double u_integ = 0;
 } Disc_Integ;
 
+// структура реле
+typedef struct Relay {
+  // сигнал от реле
+  double u_relay = 0;
+} Relay;
+
+// структура Ф от тау
+typedef struct F_ot_tau {
+  // переменные для тау
+  // сигнал сохранённый в тау
+  double u_tau = 0;
+  // Время ВКЛючения двигателя
+  unsigned long t_on = 0;
+  // Время ВЫКЛючения двигателя
+  unsigned long t_off = 0;
+  // Логическая переменная (Работает ли двигатель?) - ДА, НЕТ
+  // По умолчанию - НЕТ - не работает
+  bool is_engine_work = false;
+  // Счётчик, сколько раз запускался двигатель
+  // По умолчанию 0, так как ни разу еще не запускался
+  unsigned long cnt_start = 0;
+} F_ot_tau;
 
 // Глобальные переменные
-
-double u = 0;
-
-// переменные используемые внутри дискретного интегратора
+// переменная типа структуры сумматора
+Sum sum;
+// переменная типа структуры коэффициента усиления
+Gain gain;
+// переменная типа структуры насыщения
+Saturation sat;
+// переменная типа структуры дискретного интегратора
 Disc_Integ integrator;
-//double t_before = 0;
-// сигнал от интегратора
-//double u_integ = 0;
-
-// сигнал от реле
-double u_reley = 0;
-
-// переменные для тау
-// сигнал сохранённый в тау
-double u_tau = 0;
-// Время ВКЛючения двигателя
-unsigned long t_on = 0;
-// Время ВЫКЛючения двигателя
-unsigned long t_off = 0;
-// Логическая переменная (Работает ли двигатель?) - ДА, НЕТ
-// По умолчанию - НЕТ - не работает
-bool is_engine_work = false;
-// Счётчик, сколько раз запускался двигатель
-//  По умолчанию 0, так как ни разу еще не запускался
-unsigned long cnt_start = 0;
-
+// переменная типа структуры реле
+Relay relay;
+// переменная типа структуры Ф от тау
+F_ot_tau ft;
 // Флаг что был сигнал stop
 bool isStop = false;
 // Данные с ПК
 String pc_data;
 
-/*Задать глобальные переменные по умолчанию */
-/*void ResetVariables()
-{
-  // значение сигнала
-  u = 0;
-
-  // переменные используемые внутри дискретного интегратора
-  t_before = 0;
-  // сигнал от интегратора
-  u_integ = 0;
-
-  // сигнал от реле
-  u_reley = 0;
-  // переменные для тау
-  // сигнал сохранённый в тау
-  u_tau = 0;
-  // Время ВКЛючения двигателя
-  t_on = 0;
-  // Время ВЫКЛючения двигателя
-  t_off = 0;
-  // Логическая переменная (Работает ли двигатель?) - ДА, НЕТ
-  // По умолчанию - НЕТ - не работает
-  is_engine_work = false;
-  // Счётчик, сколько раз запускался двигатель
-  //  По умолчанию 0, так как ни разу еще не запускался
-  cnt_start = 0;
-
-  // Флаг что был сигнал stop
-  isStop = false;
-}
-*/
 /* Дискретный интегратор */
 /* Подаём на вход сигнал */
-// k - коэффициент
 // in_u - значение сигнала на входе в интегратор
 // t - текущее время
 void DiscreteIntegratorIn(double in_u, unsigned long t) {
 
   // Параметры дискретного интегратора
+  // k - коэффициент
   double K = 1;
   // Шаг дискретизации
   unsigned long T = 10;
@@ -84,55 +80,51 @@ void DiscreteIntegratorIn(double in_u, unsigned long t) {
   // Если мы ждали больше шага дискретизации
   if (t - integrator.t_before >= T) {
     // Обновляем значение интеграла
-    integrator.u += in_u * K * T * 0.001;
+    integrator.u_integ += in_u * K * T * 0.001;  // мс
     // Обновляем сохранённое время
     integrator.t_before = t;
   }
   // Если еще не прождали нужный шаг дискретизации
   // То ничего не делаем, и выводим предыдущий результат
 
-  u = integrator.u;
+  //u = integrator.u_integ;
 }
 /* Выходная линия с интеграла */
 double DiscreteIntegratorOut() {
-  
-  return integrator.u;
+
+  return integrator.u_integ;
 }
 
 // Summator
 void SummatorIn2(double u1, double u2) {
   // Значение сигнала, полученного Сумматором
-
-  u = u1 + u2;
+  sum.u_sum = u1 + u2;
 }
 
 /* Выходная линия с сумматора */
 double SummatorOut2() {
-  return u;
+  return sum.u_sum;
 }
 void SummatorIn3(double u1, double u2, double u3) {
   // Значение сигнала, полученного Сумматором
-
-  u = u1 + u2 + u3;
+  sum.u_sum = u1 + u2 + u3;
 }
 
 /* Выходная линия с сумматора */
 double SummatorOut3() {
-  return u;
+  return sum.u_sum;
 }
 
-/*Класс Множителя, умножает сигнал на какое-то число*/
 // Gain
 void GainIn(double in_u) {
   double multiplier = 0.1;
-  u = in_u * multiplier;
+  gain.u_gain = in_u * multiplier;
 }
 
 double GainOut() {
-  return u;
+  return gain.u_gain;
 }
 
-/* Класс для насыщения - обрезки значений сигнала */
 // Saturation
 void SaturationIn(double in_u) {
   // Верхняя граница сигнала
@@ -141,20 +133,19 @@ void SaturationIn(double in_u) {
   double lower = -0.25;
 
   if (in_u >= upper) {
-    u = upper;
+    sat.u_saturation = upper;
   } else if (in_u <= lower) {
-    u = lower;
+    sat.u_saturation = lower;
   } else {
-    u = in_u;
+    sat.u_saturation = in_u;
   }
 }
 
 /* выходная линия */
 double SaturationOut() {
-  return u;
+  return sat.u_saturation;
 }
 
-/* Класс представляет собой Реле */
 // Relay
 void RelayIn(double in_u, double sp, double an) {
 
@@ -169,20 +160,18 @@ void RelayIn(double in_u, double sp, double an) {
 
   // Преобразуем сигнал либо к -1, либо 0, либо 1
   if (in_u >= positive) {
-    u_reley = 1;
+    relay.u_relay = 1;
   } else if (in_u <= negative) {
-    u_reley = -1;
+    relay.u_relay = -1;
   } else {
     // u принадлежит интервалу (negative, positive )
-    u_reley = 0;
+    relay.u_relay = 0;
   }
-
-  u = u_reley;
 }
 
 /* Сигнал который получаем на выходе из Реле */
 double RelayOut() {
-  return u_reley;
+  return relay.u_relay;
 }
 
 // Regulator
@@ -211,46 +200,46 @@ void F_ot_tauIn(unsigned long t, double in_u) {
 
   // Проверяем работает ли двигатель
   // Если работает
-  if (is_engine_work) {
+  if (ft.is_engine_work) {
     // Находим время работы двигателя
     // Это текущее время минус время запуска двигателя
-    unsigned long wake_time = t - t_on;
+    unsigned long wake_time = t - ft.t_on;
     // Если  время работы двигателя >30мс
     if (wake_time > tau) {
       // Нужно проверить значение сигнала
       // Если значение сигнала не равно сигналу при включении двигателя
-      if (in_u != u_tau) {
+      if (in_u != ft.u_tau) {
         // Нужно выключить двигатель
-        is_engine_work = false;
+        ft.is_engine_work = false;
         // Задаём время выключения двигателя - теущее
-        t_off = t;
+        ft.t_off = t;
         // сохранить значение сигнала, ставим именно 0
         // Потому что у нас ступенчатый вид сигнала
         // И должна быть его 0 фаза
         // Не может быть чтобы сигнал был,
         // например 0.5 и сразу стал -0.5
-        u_tau = 0;
+        ft.u_tau = 0;
       }
     }
   } else {
     // Двигатель не работает
     // Проверяем запускался ли он хоть раз
     // Если еще не запускался
-    if (cnt_start == 0) {
+    if (ft.cnt_start == 0) {
       // То нам не нужно вычислять время спячки двигателя
       // И как только сигнал будет не нулевой
       // Включаем двигатель
       // если текущее значение сигнала НЕ нулевое
       if (in_u != 0) {
         // Включаем двигатель
-        is_engine_work = true;
+        ft.is_engine_work = true;
         // Увеличиваем счётчик запусков двигателей
-        cnt_start += 1;
+        ft.cnt_start += 1;
         // сохраняем время запуска двигателя - текущее время
-        t_on = t;
+        ft.t_on = t;
         // сохранить значение сигнала двигателя при старте
         // - то есть текущее значенеи сигнала
-        u_tau = in_u;
+        ft.u_tau = in_u;
       }
     } else {
       // Двигатель УЖЕ запускался
@@ -258,26 +247,24 @@ void F_ot_tauIn(unsigned long t, double in_u) {
       // Если сигнал НЕ нулевой
       if (in_u != 0) {
         // Включаем двигатель
-        is_engine_work = true;
+        ft.is_engine_work = true;
         // Увеличиваем счётчик запусков двигателей
-        cnt_start += 1;
+        ft.cnt_start += 1;
         // сохраняем время запуска двигателя - текущее время
-        t_on = t;
+        ft.t_on = t;
         // сохранить значение сигнала двигателя при старте
         // - то есть текущее значение сигнала
-        u_tau = in_u;
+        ft.u_tau = in_u;
       }
     }
   }
-  u = u_tau;
 }
 
 double F_ot_tauOut() {
-  return u;
+  return ft.u_tau;
 }
 
-/*Класс системы на Arduino*/
-// System
+// System Система из регулятора и Ф от тау
 
 void SystemIn(String data) {
   // data - это строка, где храниться время, скорость и угол через пробел
@@ -317,15 +304,12 @@ void outPrintln() {
 
 void setup() {
   SystemIn(pc_data);
-  // задаём значения для глобальных переменых по умолчанию
-  // ResetVariables();
   // Настройка вывода
   // инициирует последовательное соединение
   // и задает скорость передачи данных в бит/с (бод)
   Serial.begin(115200);
   // Отправляем сигнал, что готовы к работе
   Serial.println("OK");
- 
 }
 
 void loop() {
